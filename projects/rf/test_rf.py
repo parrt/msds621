b0a4284462968df8228de625e3a4c70f3bed8dfa
parrt <parrt@cs.usfca.edu>
2019-11-25 15:39:31 -0800

    update test rig for RF

--- projects/rf/test_rf.py
+++ projects/rf/test_rf.py
@@ -19,39 +19,39 @@ def test_boston_oob():
 
 def test_boston_min_samples_leaf():
     X, y = load_boston(return_X_y=True)
-    run_regression_test(X, y, ntrials=5, min_samples_leaf=5, min_training_score=.90, grace=0.08)
+    run_regression_test(X, y, ntrials=5, min_samples_leaf=5, grace=0.08)
 
 def test_boston_all_features():
     X, y = load_boston(return_X_y=True)
-    run_regression_test(X, y, ntrials=3, max_features=1.0, min_training_score=.90, grace=0.08)
+    run_regression_test(X, y, ntrials=3, max_features=1.0, grace=0.08)
 
 def test_boston_most_features():
     X, y = load_boston(return_X_y=True)
-    run_regression_test(X, y, ntrials=4, max_features=2/3, min_training_score=.90, grace=0.08)
+    run_regression_test(X, y, ntrials=4, max_features=2/3, grace=0.08)
 
 def test_boston_min_samples_leaf_oob():
     X, y = load_boston(return_X_y=True)
-    run_regression_test(X, y, ntrials=5, min_samples_leaf=5, min_training_score=.90, grace=0.08, oob=True)
+    run_regression_test(X, y, ntrials=5, min_samples_leaf=5, grace=0.08, oob=True)
 
 def test_diabetes():
     X, y = load_diabetes(return_X_y=True)
-    run_regression_test(X, y, min_training_score = .83, grace=0.11)
+    run_regression_test(X, y, min_training_score = .72, grace=0.11)
 
 def test_diabetes_ntrees():
     X, y = load_diabetes(return_X_y=True)
-    run_regression_test(X, y, min_training_score = .83, grace=0.11, n_estimators=25)
+    run_regression_test(X, y, min_training_score = .72, grace=0.11, n_estimators=25)
 
 def test_diabetes_all_features():
     X, y = load_diabetes(return_X_y=True)
-    run_regression_test(X, y, min_training_score = .83, grace=0.11, max_features=1.0)
+    run_regression_test(X, y, min_training_score = .72, grace=0.11, max_features=1.0)
 
 def test_diabetes_most_features():
     X, y = load_diabetes(return_X_y=True)
-    run_regression_test(X, y, min_training_score = .83, grace=0.11, max_features=2/3)
+    run_regression_test(X, y, min_training_score = .72, grace=0.11, max_features=2/3)
 
 def test_diabetes_oob():
     X, y = load_diabetes(return_X_y=True)
-    run_regression_test(X, y, min_training_score = .83, grace=0.11, oob=True)
+    run_regression_test(X, y, min_training_score = .72, grace=0.11, oob=True)
 
 def test_california_housing():
     X, y = fetch_california_housing(return_X_y=True)
@@ -64,23 +64,23 @@ def test_california_housing_oob():
 
 def test_iris_ntrees():
     X, y = load_iris(return_X_y=True)
-    run_classification_test(X, y, ntrials=5, min_training_score=0.96, n_estimators=25)
+    run_classification_test(X, y, ntrials=5, min_training_score=0.93, n_estimators=25)
 
 def test_iris():
     X, y = load_iris(return_X_y=True)
-    run_classification_test(X, y, ntrials=5, min_training_score=0.96)
+    run_classification_test(X, y, ntrials=5, min_training_score=0.93)
 
 def test_iris_all_features():
     X, y = load_iris(return_X_y=True)
-    run_classification_test(X, y, ntrials=5, min_training_score=0.96, max_features=1.0)
+    run_classification_test(X, y, ntrials=5, min_training_score=0.93, max_features=1.0)
 
 def test_iris_most_features():
     X, y = load_iris(return_X_y=True)
-    run_classification_test(X, y, ntrials=5, min_training_score=0.96, max_features=2/3)
+    run_classification_test(X, y, ntrials=5, min_training_score=0.93, max_features=2/3)
 
 def test_iris_oob():
     X, y = load_iris(return_X_y=True)
-    run_classification_test(X, y, ntrials=5, min_training_score=0.96, oob=True)
+    run_classification_test(X, y, ntrials=5, min_training_score=0.93, oob=True)
 
 def test_wine():
     X, y = load_wine(return_X_y=True)
@@ -115,7 +115,7 @@ def test_breast_cancer_oob():
     run_classification_test(X, y, ntrials=5, min_training_score=0.96, oob=True)
 
 
-def run_regression_test(X, y, ntrials=2, min_training_score = .90, min_samples_leaf=3, max_features=0.3, grace=.08, oob=False, n_estimators=18):
+def run_regression_test(X, y, ntrials=2, min_training_score = .85, min_samples_leaf=3, max_features=0.3, grace=.08, oob=False, n_estimators=18):
     stack = inspect.stack()
     caller_name = stack[1].function[len('test_'):]
     X = X[:500]
@@ -152,6 +152,14 @@ def run_regression_test(X, y, ntrials=2, min_training_score = .90, min_samples_l
         else:
             sklearn_oob_scores.append(0.0)
 
+    print()
+    if oob:
+        print(f"{caller_name}: 621 OOB score {np.mean(oob_scores):.2f} vs sklearn OOB {np.mean(sklearn_oob_scores):.2f}")
+    print(f"{caller_name}: 621     Train R^2 score mean {np.mean(train_scores):.2f}, stddev {np.std(train_scores):3f}")
+    print(f"{caller_name}: Sklearn Train R^2 score mean {np.mean(sklearn_train_scores):.2f}, stddev {np.std(sklearn_train_scores):3f}")
+    print(f"{caller_name}: 621     Test  R^2 score mean {np.mean(scores):.2f}, stddev {np.std(scores):3f}")
+    print(f"{caller_name}: Sklearn Test  R^2 score mean {np.mean(sklearn_scores):.2f}, stddev {np.std(sklearn_scores):3f}")
+
     assert np.mean(train_scores) >= min_training_score, \
            f"Training R^2: {np.mean(train_scores):.2f} must be >= {min_training_score}"
     assert np.mean(scores)+grace >= np.mean(sklearn_scores), \
@@ -160,14 +168,8 @@ def run_regression_test(X, y, ntrials=2, min_training_score = .90, min_samples_l
         assert np.abs(np.mean(oob_scores) - np.mean(sklearn_oob_scores)) < grace, \
             f"OOB R^2: {np.mean(oob_scores):.2f} must be within {grace:2f} of sklearn score: {np.mean(sklearn_oob_scores):.2f}"
 
-    print()
-    if oob:
-        print(f"{caller_name}: 621 OOB score {np.mean(oob_scores):.2f} vs sklearn OOB {np.mean(sklearn_oob_scores):.2f}")
-    print(f"{caller_name}: 621 R^2 score {np.mean(train_scores):.2f}, {np.mean(scores):.2f}")
-    print(f"{caller_name}: Sklearn R^2 score {np.mean(sklearn_train_scores):.2f}, {np.mean(sklearn_scores):.2f}")
 
-
-def run_classification_test(X, y, ntrials=1, min_samples_leaf=3, max_features=0.3, min_training_score=1.0, grace=.05, oob=False, n_estimators=15):
+def run_classification_test(X, y, ntrials=1, min_samples_leaf=3, max_features=0.3, min_training_score=1.0, grace=.07, oob=False, n_estimators=15):
     stack = inspect.stack()
     caller_name = stack[1].function[len('test_'):]
     X = X[:500]
@@ -206,7 +208,7 @@ def run_classification_test(X, y, ntrials=1, min_samples_leaf=3, max_features=0.
 
     if oob:
         assert np.abs(np.mean(scores)- np.mean(sklearn_scores)) < grace, \
-               f"OOB R^2: {np.mean(oob_scores):.2f} must be within {grace:.2f} of sklearn score: {np.mean(sklearn_oob_scores):.2f}"
+               f"OOB accuracy: {np.mean(oob_scores):.2f} must be within {grace:.2f} of sklearn score: {np.mean(sklearn_oob_scores):.2f}"
     assert np.mean(train_scores) >= min_training_score, \
            f"Training accuracy: {np.mean(train_scores):.2f} must {min_training_score:.2f}"
     assert np.mean(scores)+grace >= np.mean(sklearn_scores), \
